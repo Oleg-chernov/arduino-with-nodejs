d9cda7c210b304795bdc663f14159b0dc45dc96c
Oleg-chernov <razrab404@gmail.com>
2018-02-25 13:52:57 +0300

    Main code

---
+++ public/js/main.js
@@ -0,0 +1,33 @@
+var dataArduino = []
+
+
+
+setInterval(function(){
+	$.ajax({
+		type: 'POST',
+		url: '/data',
+		data: 'no',
+		success: function (data) {
+			dataArduino.push(Number(data));
+	
+			new Chartist.Line('.ct-chart', {
+			  labels: dataArduino,
+			  series: [
+					dataArduino
+			  ]
+			},  {
+			  fullWidth: false,
+			  chartPadding: {
+				right: 40
+			  }});
+		},
+		error: function (xhr, str) {
+			alert(xhr.responseCode);
+		}
+	});
+	console.log(dataArduino)
+}, 500)
+
+document.querySelector('#clear').onclick = () => {
+	dataArduino = [];
+}
--- serial.js
+++ serial.js
@@ -1,9 +1,24 @@
+var express = require("express");
 var SerialPort = require("serialport");
 var serialport = new SerialPort("/dev/ttyUSB0");
 
+var dataArduino = '';
+var app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+app.post("/data", function(request, response){
+	response.send(dataArduino);
+});
+
+app.get("/", function(request, response){
+     response.send("<h1>Home page</h1>");
+});
+
 serialport.on('open', function () {
 	console.log('Serial Port Opend');
-	serialport.on('data', function (data) {
-		console.log(data.toString('utf8'));
-	});
 });
+	serialport.on('data', function (data) {
+		dataArduino = data.toString('utf8');
+	});
+app.listen(3000);
